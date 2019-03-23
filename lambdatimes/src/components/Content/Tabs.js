145869c5d5d741b35ddce0e1e34f2568f7a692c8
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-23 10:26:17 -0700

    Made cards appear on the screen, fixed error with no keys when mapping

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,7 +5,7 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-          { props.tabs.map(tab => (<Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} />)) }        
+          { props.tabs.map((tab, index) => (<Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} key={index} />)) }        
           {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
